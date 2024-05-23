f3a09eaa4b39b3af67d47eff5c04fb894b9fe175
k.damyanov <k.damyanov@is-bg.net>
2024-05-23 16:13:33 +0300

    Added computed

---
+++ Demos/demo_3/app.js
@@ -0,0 +1,39 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: "",
+    };
+  },
+  computed: {
+    fullName(){
+      if (this.name === "") {
+        return "";
+      }
+      return this.name + " " + "Damyanov";
+    }
+  },
+  methods: {
+    outputFullName() {
+      if (this.name === "") {
+        return "";
+      }
+      return this.name + " " + "Damyanov";
+    },
+    setName(event) {
+      this.name = event.target.value;
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
+      // this.counter--;
+    },
+    resetInput() {
+      this.name = "";
+    },
+  },
+});
+
+app.mount("#events");
