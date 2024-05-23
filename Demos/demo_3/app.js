a90a1a6a9277cd17f625ae7aa00ff61c5cd03f86
k.damyanov <k.damyanov@is-bg.net>
2024-05-23 17:22:54 +0300

    Added new tastks

--- Demos/demo_3/app.js
+++ Demos/demo_3/app.js
@@ -3,7 +3,26 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name: "",
+      fullName: "",
     };
+  },
+  watch: {
+    // name(value) {
+    //   if (this.name === "") {
+    //     return "";
+    //   }
+    //   return this.name + " " + "Damyanov";
+    // },
+    counter(value){
+      if(value >50) {
+        const that = this
+        setTimeout(function () {
+          that.counter = 0;
+        }, 1000)
+      } else if (value < 0) {
+        this.counter = 0;
+      }
+    }
   },
   computed: {
     fullName(){
