a8dbb0db02612c99c28461458a222df4dd1732dc
k.damyanov <k.damyanov@is-bg.net>
2024-05-23 13:04:39 +0300

    Added function value to <p>

--- App/app.js
+++ App/app.js
@@ -1,8 +1,18 @@
 const app = Vue.createApp({
   data() {
     return {
-      courseGoal: 'Learn Vue!',
-      vueLink: 'https://vuejs.org/'
+      courseGoal: "Learn Vue!",
+      vueLink: "https://vuejs.org/",
+    };
+  },
+  methods: {
+    outputGoal(){
+      const randNum = Math.random();
+      if (randNum < 0.5) {
+        return "Learn Vue!"
+      } else {
+        return "Master Vue!"
+      }
     }
   },
 });
