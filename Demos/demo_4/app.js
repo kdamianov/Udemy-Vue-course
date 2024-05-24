8a1bbf64a35e5e8730008eef71575cbeeadaef2d
kdamianov <kdamianov@gmail.com>
2024-05-24 17:37:49 +0300

    Added dynamic classes

--- Demos/demo_4/app.js
+++ Demos/demo_4/app.js
@@ -6,13 +6,24 @@ const app = Vue.createApp({
       boxCSelected: false,
     };
   },
+  computed: {
+    boxAClasses(){
+        return {active: this.boxASelected}
+    },
+    boxBClasses(){
+        return {active: this.boxBSelected}
+    },
+    boxCClasses(){
+        return {active: this.boxCSelected}
+    },
+  },
   methods: {
     boxSelected(box) {
       if (box === "A") {
         this.boxASelected = !this.boxASelected;
       } else if (box === "B") {
         this.boxBSelected = !this.boxBSelected;
-      } else if (box === 'C'){
+      } else if (box === "C") {
         this.boxCSelected = !this.boxCSelected;
       }
     },
