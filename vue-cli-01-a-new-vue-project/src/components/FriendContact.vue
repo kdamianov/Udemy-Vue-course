e2c572cd34c88b822f923f530950ca8559b5405d
k.damyanov <k.damyanov@is-bg.net>
2024-06-21 16:20:42 +0300

    Added more component communication

--- vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -1,17 +1,23 @@
 <template>
   <li>
-    <h2>{{ name }} {{ friendIsFavorite ? "(Favorite)" : "" }}</h2>
+    <h2>{{ name }} {{ isFavorite ? "(Favorite)" : "" }}</h2>
     <button @click="toggleFavorite">
       {{ !friendIsFavorite ? "Set" : "Remove" }} Favorite
     </button>
     <button @click="toggleDetails">
       {{ detailsAreVisible ? "Hide" : "Show" }} Details
     </button>
-
     <ul v-if="detailsAreVisible">
-      <li><strong>Phone: </strong>{{ phoneNumber }}</li>
-      <li><strong>Email: </strong>{{ emailAddress }}</li>
+      <li>
+        <strong>Phone: </strong>
+        {{ phoneNumber }}
+      </li>
+      <li>
+        <strong>Email: </strong>
+        {{ emailAddress }}
+      </li>
     </ul>
+    <button @click="deleteFriend">Delete</button>
   </li>
 </template>
 
@@ -19,6 +25,10 @@
 export default {
   // props: ["name", "phoneNumber", "emailAddress", "isFavorite"],
   props: {
+    id: {
+      type: String,
+      required: true,
+    },
     name: {
       type: String,
       required: true,
@@ -37,10 +47,21 @@ export default {
       default: false, // default value when the property is missing
     },
   },
+  // defines the events that are emited to the parent vue
+  emits: ["toggle-favorite", "delete-friend"],
+  // emits: {
+  //   "toggle-favorite": function (id) {
+  //     if (id) {
+  //       return true;
+  //     } else {
+  //       console.warn("ID is missing!");
+  //       return false;
+  //     }
+  //   },
+  // },
   data() {
     return {
       detailsAreVisible: false,
-      friendIsFavorite: this.isFavorite,
     };
   },
   methods: {
@@ -48,73 +69,11 @@ export default {
       this.detailsAreVisible = !this.detailsAreVisible;
     },
     toggleFavorite() {
-      this.friendIsFavorite = !this.friendIsFavorite;
+      this.$emit("toggle-favorite", this.id);
     },
+    deleteFriend(){
+      this.$emit("delete-friend", this.id)
+    }
   },
 };
 </script>
-<style>
-* {
-  box-sizing: border-box;
-}
-
-html {
-  font-family: "Jost", sans-serif;
-}
-
-body {
-  margin: 0;
-}
-
-header {
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  margin: 3rem auto;
-  border-radius: 10px;
-  padding: 1rem;
-  background-color: #58004d;
-  color: white;
-  text-align: center;
-  width: 90%;
-  max-width: 40rem;
-}
-
-#app ul {
-  margin: 0;
-  padding: 0;
-  list-style: none;
-}
-
-#app li {
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  margin: 1rem auto;
-  border-radius: 10px;
-  padding: 1rem;
-  text-align: center;
-  width: 90%;
-  max-width: 40rem;
-}
-
-#app h2 {
-  font-size: 2rem;
-  border-bottom: 4px solid #ccc;
-  color: #58004d;
-  margin: 0 0 1rem 0;
-}
-
-#app button {
-  font: inherit;
-  cursor: pointer;
-  border: 1px solid #ff0077;
-  background-color: #ff0077;
-  color: white;
-  padding: 0.05rem 1rem;
-  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
-}
-
-#app button:hover,
-#app button:active {
-  background-color: #ec3169;
-  border-color: #ec3169;
-  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
-}
-</style>
