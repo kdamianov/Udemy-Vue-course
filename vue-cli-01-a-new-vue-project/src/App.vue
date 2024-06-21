de6ab6cd6e308e144037630a48bae7543e82691d
k.damyanov <k.damyanov@is-bg.net>
2024-06-21 13:58:51 +0300

    Added props

--- vue-cli-01-a-new-vue-project/src/App.vue
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -4,7 +4,17 @@
       <h1>My friends</h1>
     </header>
     <ul>
-      <friend-contact></friend-contact>
+      <friend-contact
+        name="Manuel Lorenz"
+        email-address="manuel@localhost.com"
+        phone-number="0123 456789"
+      ></friend-contact>
+      <friend-contact
+        name="Julie Jones"
+        email-address="julie@localhost.com"
+        phone-number="9876 543210"
+        is-favorite="1"
+      ></friend-contact>
     </ul>
   </section>
 </template>
