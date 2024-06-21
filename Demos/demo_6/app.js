5c390e7b634af805b50c0d11b1a38d40c33780a3
k.damyanov <k.damyanov@is-bg.net>
2024-06-21 10:08:43 +0300

    Added new demo for Components

---
+++ Demos/demo_6/app.js
@@ -0,0 +1,33 @@
+const app = Vue.createApp({});
+app.component("friend-contact", {
+  template: `
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails()">
+        {{ detailsAreVisible ? 'Show' : 'Hide' }} details
+    </button>
+    <ul v-if="detailsAreVisible">
+        <li><strong>Phone:</strong> {{ friend.phone }}</li>
+        <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+  `,
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "manuel",
+        name: "Manuel Lorenz",
+        phone: "01234 5678 991",
+        email: "manuel@localhost.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+});
+
+app.mount("#app");
