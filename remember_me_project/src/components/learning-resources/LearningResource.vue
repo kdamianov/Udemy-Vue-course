68710d28e0fcd7b160226c5c707bb55bc96231e3
k.damyanov <k.damyanov@is-bg.net>
2024-07-04 12:15:20 +0300

    Added BaseDialog and Delete function

--- remember_me_project/src/components/learning-resources/LearningResource.vue
+++ remember_me_project/src/components/learning-resources/LearningResource.vue
@@ -3,21 +3,20 @@
     <base-card>
       <header>
         <h3>{{ title }}</h3>
-        <base-button mode="flat">Delete</base-button>
+        <base-button mode="flat" @click="deleteResource(id)">Delete</base-button>
       </header>
       <p>{{ description }}</p>
       <nav>
-        <a :href="link">View resource</a>
+        <a :href="link">View Resource</a>
       </nav>
     </base-card>
   </li>
 </template>
 
 <script>
-import BaseButton from '../UI/BaseButton.vue';
 export default {
-  components: { BaseButton },
-  props: ['title', 'description', 'link'],
+  props: ['id', 'title', 'description', 'link'],
+  inject: ['deleteResource']
 };
 </script>
 
--- remember_me_project/src/components/learning-resources/StoredResources.vue
+++ remember_me_project/src/components/learning-resources/StoredResources.vue
@@ -3,6 +3,7 @@
     <learning-resource
       v-for="res in resources"
       :key="res.id"
+      :id="res.id"
       :title="res.title"
       :description="res.description"
       :link="res.link"
@@ -12,12 +13,13 @@
 
 <script>
 import LearningResource from './LearningResource.vue';
+
 export default {
-  props: ['resources'],
+  inject: ['resources'],
   components: {
-    LearningResource,
-  },
-};
+    LearningResource
+  }
+}
 </script>
 
 <style scoped>
