9ea9cd1ef1bf21139e67550ddc13763a4332c717
Калин Дамянов <k.damyanov@is-bg.net>
2025-01-21 17:14:31 +0200

    Redirecting and Catch all routes

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -5,6 +5,7 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMember from './components/teams/TeamMembers.vue';
+import NotFound from './components/nav/NotFound.vue';
 
 const app = createApp(App)
 const router = createRouter({
@@ -12,18 +13,14 @@ const router = createRouter({
     //we register all the pats
     routes: [
         //set the component for the specific path
-        {
-            path: '/teams',
-            component: TeamsList
-        },
-        {
-            path: '/users',
-            component: UsersList
-        }
-        ,
+        //same as "alias: '/'", but it loads different path!
+        { path: '/', redirect: '/teams' },
+        { path: '/teams', component: TeamsList },
+        { path: '/users', component: UsersList },
         //with 'props: true' we define that the dynamic param will be passed as props!
         { path: '/teams/:teamId', component: TeamMember, props: true},
-
+        //dynamic segment for any character combination, must be las, not to override all the above
+        { path: '/:notFound(.*)', component: NotFound},
     ]
 });
 
