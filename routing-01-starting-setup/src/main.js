c1f1a1c8588333f2b1b74619a8cb87b8a69b3e30
Калин Дамянов <k.damyanov@is-bg.net>
2025-03-12 16:36:09 +0200

    Deleted routing-01-starting-setup.zip

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -6,28 +6,40 @@ import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
 import NotFound from './components/nav/NotFound.vue';
+import TeamsFooter from './components/teams/TeamsFooter.vue';
+import UsersFooter from './components/users/UsersFooter.vue';
 
+const app = createApp(App)
 const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    { path: '/', redirect: '/teams' },
-    {
-      name: 'teams',
-      path: '/teams',
-      component: TeamsList,
-      children: [
-        { name: 'team-members', path: ':teamId', component: TeamMembers, props: true } // /teams/t1
-      ]
-    }, // our-domain.com/teams => TeamsList
-    { path: '/users', component: UsersList },
+    history: createWebHistory(),
+    //we register all the pats
+    routes: [
+        //set the component for the specific path
+        //same as "alias: '/'", but it loads different path!
+        { path: '/', redirect: '/teams' },
+        {
+            name: 'teams',
+            path: '/teams',
+            components: {
+                default: TeamsList,
+                footer: TeamsFooter
+            },
+            children: [
+                { name: 'team-members', path: ':teamId', component: TeamMembers, props: true },
+            ]
+        },
+        {
+            path: '/users', components: {
+                default: UsersList,
+                footer: UsersFooter
+            }
+        },
+        //with 'props: true' we define that the dynamic param will be passed as props!
 
-    { path: '/:notFound(.*)', component: NotFound }
-  ],
-  linkActiveClass: 'active'
+        //dynamic segment for any character combination, must be las, not to override all the above
+        { path: '/:notFound(.*)', component: NotFound },
+    ]
 });
 
-const app = createApp(App);
-
 app.use(router);
-
 app.mount('#app');
